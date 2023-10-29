80201b46d453f7939dde40b0ceb598359e23cb75
Liakhovskyi Vladyslav <vlad@revotale.com>
2023-10-29 16:18:12 +0200

    Commit initial generator config

---
+++ jane-initial-config.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+// Config used to generate initial code of a library
+return [
+    'openapi-file' => 'https://api.checkbox.in.ua/api/openapi.json',
+    'namespace' => 'Vendor\Library\Generated\CheckboxUA',
+    'directory' => __DIR__.'/generated/checkbox_ua',
+    'throw-unexpected-status-code' => true,
+    'date-format' => 'Y-m-d\\TH:i:s.uP',
+    'clean-generated' => true,
+];
