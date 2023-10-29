22b76eb9c0dd044a86297207960f0eec89bcf746
Liakhovskyi Vladyslav <vlad@revotale.com>
2023-10-29 15:54:51 +0200

    Install php cs fixer

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+$finder = (new PhpCsFixer\Finder())
+    ->in(__DIR__)
+    ->exclude('var');
+
+return (new PhpCsFixer\Config())
+    ->setRules([
+        '@Symfony' => true,
+        'global_namespace_import' => [
+            'import_classes' => true,
+            'import_constants' => true,
+            'import_functions' => true,
+        ],
+    ])
+    ->setFinder($finder);
