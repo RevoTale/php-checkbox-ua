22d334dde9c4664bea7501c0b9a3ec578d5bc4ee
Liakhovskyi Vladyslav <rusted@revotale.com>
2024-01-31 23:34:11 +0200

    Fix badly generated code.

--- src/Runtime/Normalizer/ReferenceNormalizer.php
+++ src/Runtime/Normalizer/ReferenceNormalizer.php
@@ -12,13 +12,20 @@ class ReferenceNormalizer implements NormalizerInterface
     public function normalize(mixed $object, ?string $format = null, array $context = []): array|string|int|float|bool|\ArrayObject|null
     {
         $ref = [];
-        $ref['$ref'] = (string) $object->getReferenceUri();
+        $ref['$ref'] = (string)$object->getReferenceUri();
 
         return $ref;
     }
 
-    public function supportsNormalization($data, $format = null): bool
+    public function supportsNormalization($data, $format = null, array $context = []): bool
     {
         return $data instanceof Reference;
+    }
+
+    public function getSupportedTypes(?string $format): array
+    {
+        return [
+            Reference::class
+        ];
     }
 }
