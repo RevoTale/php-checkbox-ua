152b422c8a88603225b65a4d171aaee2f69c7245
Liakhovskyi Vladyslav <rusted@revotale.com>
2024-03-08 22:27:07 +0200

    Allow a custom client to be passed.

--- src/ClientFactory.php
+++ src/ClientFactory.php
@@ -7,19 +7,22 @@ namespace RevoTale\CheckboxUA;
 use GuzzleHttp\Psr7\Uri;
 use Http\Client\Common\Plugin\AddHostPlugin;
 use Http\Client\Common\PluginClient;
+use Http\Client\HttpAsyncClient;
 use Http\Discovery\Psr18ClientDiscovery;
 use Jane\Component\OpenApiRuntime\Client\Plugin\AuthenticationRegistry;
+use Psr\Http\Client\ClientInterface;
 
 final readonly class ClientFactory
 {
-    public static function createClient(string $token = null): Client
+    public static function createClient(string                          $token = null,
+                                        HttpAsyncClient|ClientInterface $client = null): Client
     {
         $plugins = [];
         if (null !== $token) {
             $plugins[] = new AuthenticationRegistry([new BearerAuthentication($token)]);
         }
         $plugins[] = new AddHostPlugin(new Uri('https://api.checkbox.in.ua'), ['replace' => true]);
-        $pluginClient = new PluginClient(Psr18ClientDiscovery::find(), $plugins);
+        $pluginClient = new PluginClient($client??Psr18ClientDiscovery::find(), $plugins);
 
         return Client::create($pluginClient);
     }
